0d7c7158a54f0efbc81e7ba3d3a20b60eb56c5d1
Ayoub <ziocaccaputo@gmail.com>
2025-11-06 19:04:17 +0100

    inizio programma tris

--- FarahA-LAB5/reverseInPlace.cpp
+++ FarahA-LAB5/reverseInPlace.cpp
@@ -4,25 +4,39 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
     int x;
-    cout<<"Inserisci il numero di elementi del vector: ";
-    cin>>x;
-    vector <int> v(x);
+    cout << "Inserisci il numero di elementi del vector: ";
+    cin >> x;
+    
+    vector<int> v(x);
     int n;
-    int size = int (v.size());
-    //inserisco i numeri nel vector
-    for (int i=0; i<size; i++)
+    int size = v.size();
+
+    // Inserimento dei numeri nel vector
+    for (int i = 0; i < size; i++)
     {
-        cout<<"Inserisci un numero intero v""("<<i<<")"": ";
-        cin>>n;
-        v[i]=n;
+        cout << "Inserisci un numero intero v(" << i << "): ";
+        cin >> n;
+        v[i] = n;
     }
-    for (int i=size-1; i>=0; i--)
+
+    // Inversione del vector
+    for (int i = 0; i < size / 2; i++)
     {
-        cout<<v[i]<<" ";
+        int temp = v[i];
+        v[i] = v[size - 1 - i];
+        v[size - 1 - i] = temp;
+    }
+
+    // Stampa del vector invertito
+    cout << "Vector invertito: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i] << " ";
     }
     cout << endl;
+
     return 0;
 }
