4beaea95ff70e4b0538ca370f3697c7b0d31f63d
YoobeeTM <ziocaccaputo@gmail.com>
2025-11-11 15:28:14 +0100

    merge lab6

--- FarahA-LAB6/mergeVectors.cpp
+++ FarahA-LAB6/mergeVectors.cpp
@@ -1,124 +1,122 @@
 #include <iostream>
-#include <string>
 #include <vector>
-
+#include <ctime>
 using namespace std;
 
-int main ()
+int main()
 {
-    //S1 input
     srand(time(NULL));
-    int x = (rand()%10);
-    
-    vector<int> s1(x);
-    int size1 = s1.size();
 
-    for (int i = 0; i < size1; i++)
+    int x = (rand() % 20) + 1;
+    vector<int> s1(x);
+
+    for (int i = 0; i < x; i++)
     {
-        int n1 = (rand()%256);
+        int n1 = (rand() % 255);
         s1[i] = n1;
     }
 
-    //S2 input
-    int y = (rand()%10);
-    
+    int y = (rand() % 20) + 1;
     vector<int> s2(y);
-    int size2 = s2.size();
 
-    for (int i = 0; i < size2; i++)
+    for (int i = 0; i < y; i++)
     {
-        int n2 = (rand()%256);
+        int n2 = (rand() % 255);
         s2[i] = n2;
     }
-    
-    //stampa s1 generato
-    cout<<"Vector s1 (generato) = { ";
-    for (size_t i=0; i<s1.size(); i++)
-    {
-        cout<<s1[i];
-        if (i!=s1.size()-1) cout<<", ";
-    }
-    cout<<" }"<<endl;
 
-    //stampa s1 generato
-    cout<<"Vector s2 (generato) = { ";
-    for (size_t i=0; i<s2.size(); i++)
+    cout << "Vector s1 (generato) = { ";
+    for (size_t i = 0; i < s1.size(); i++)
     {
-        cout<<s2[i];
-        if (i!=s2.size()-1) cout<<", ";
+        cout << s1[i];
+        if (i != s1.size() - 1) cout << ", ";
     }
-    cout<<" }"<<endl;
+    cout << " }" << endl;
 
-    //ordinamento s1
-    int minIndex1;
-    for (size_t i = 0; i < s1.size() - 1; i++) 
+    cout << "Vector s2 (generato) = { ";
+    for (size_t i = 0; i < s2.size(); i++)
     {
-        minIndex1 = i;
-        for (size_t j = i + 1; j < s1.size(); j++) 
+        cout << s2[i];
+        if (i != s2.size() - 1) cout << ", ";
+    }
+    cout << " }" << endl;
+
+    for (size_t i = 0; i < s1.size() - 1; i++)
+    {
+        int minIndex = i;
+        for (size_t j = i + 1; j < s1.size(); j++)
         {
-            if (s1[j] < s1[minIndex1]) 
-            {
-                minIndex1 = j;
-            }
+            if (s1[j] < s1[minIndex])
+                minIndex = j;
         }
         int temp = s1[i];
-        s1[i] = s1[minIndex1];
-        s1[minIndex1] = temp;
+        s1[i] = s1[minIndex];
+        s1[minIndex] = temp;
     }
 
-    //stampa s1 ordinato
-    cout<<"Vector s1 (ordinato) = { ";
-    for (size_t i=0; i<s1.size(); i++)
+    for (size_t i = 0; i < s2.size() - 1; i++)
     {
-        cout<<s1[i];
-        if (i!=s1.size()-1) cout<<", ";
-    }
-    cout<<" }"<<endl;
-
-    //ordinamento s2
-    int minIndex2;
-    for (size_t i = 0; i < s2.size() - 1; i++) 
-    {
-        minIndex2 = i;
-        for (size_t j = i + 1; j < s2.size(); j++) 
+        int minIndex = i;
+        for (size_t j = i + 1; j < s2.size(); j++)
         {
-            if (s2[j] < s2[minIndex2]) 
-            {
-                minIndex2 = j;
-            }
+            if (s2[j] < s2[minIndex])
+                minIndex = j;
         }
         int temp = s2[i];
-        s2[i] = s2[minIndex2];
-        s2[minIndex2] = temp;
+        s2[i] = s2[minIndex];
+        s2[minIndex] = temp;
     }
 
-    //stampa s1 ordinato
-    cout<<"Vector s2 (ordinato) = { ";
-    for (size_t i=0; i<s2.size(); i++)
+    cout << "Vector s1 (ordinato) = { ";
+    for (size_t i = 0; i < s1.size(); i++)
     {
-        cout<<s2[i];
-        if (i!=s2.size()-1) cout<<", ";
+        cout << s1[i];
+        if (i != s1.size() - 1) cout << ", ";
     }
-    cout<<" }"<<endl;
-    
-    //stampa d (s1+s2)
-    vector <int> d(0);
-    for (int i=0; i<x; i++)
+    cout << " }" << endl;
+
+    cout << "Vector s2 (ordinato) = { ";
+    for (size_t i = 0; i < s2.size(); i++)
+    {
+        cout << s2[i];
+        if (i != s2.size() - 1) cout << ", ";
+    }
+    cout << " }" << endl;
+
+    vector<int> d;
+
+    for (size_t i = 0; i < s1.size(); i++)
     {
         d.push_back(s1[i]);
     }
-    for (int i=0; i<y; i++)
+
+    int ultimo_s1 = s1[s1.size() - 1];
+
+    size_t start_index = s2.size();
+    for (size_t i = 0; i < s2.size(); i++)
     {
-        d.push_back(s2[i]);
+        if (s2[i] > ultimo_s1)
+        {
+            start_index = i;
+            break;
+        }
     }
 
-    cout<<"Vector d = { ";
-    for (size_t i=0; i<d.size(); i++)
+    if (start_index < s2.size())
     {
-        cout<<d[i];
-        if (i!=d.size()-1) cout<<", ";
+        for (size_t i = start_index; i < s2.size(); i++)
+        {
+            d.push_back(s2[i]);
+        }
     }
-    cout<<" }"<<endl;
-    
-    return 0;
+
+    cout << "Vector d (s1 + s2 da primo valore maggiore di s1) = { ";
+    for (size_t i = 0; i < d.size(); i++)
+    {
+        cout << d[i];
+        if (i != d.size() - 1) cout << ", ";
+    }
+    cout << " }" << endl;
+
+ return 0;
 }
