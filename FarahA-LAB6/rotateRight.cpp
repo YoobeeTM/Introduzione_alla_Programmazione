cc75c7dc7b1aafd0edc83d278f396ea0fc1f0ad7
YoobeeTM <ziocaccaputo@gmail.com>
2025-11-11 14:16:23 +0100

    rotate right lab6

---
+++ FarahA-LAB6/rotateRight.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    vector<int> d(5);
+
+    // genera numeri casuali
+    for (size_t i = 0; i < d.size(); i++) {
+        d[i] = rand() % 50;
+    }
+
+    // stampa il vector generato
+    cout << "[ ";
+    for (size_t i = 0; i < d.size(); i++) {
+        cout << d[i] << " ";
+    }
+    cout << "]" << endl;
+
+    // rotate right (spostamento a destra)
+    int last = d.back();
+    for (size_t i = d.size() - 1; i > 0; i--) {
+        d[i] = d[i - 1];
+    }
+    d[0] = last;
+
+    // stampa vector dopo la rotazione
+    cout << "[ ";
+    for (size_t i = 0; i < d.size(); i++) {
+        cout << d[i] << " ";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
