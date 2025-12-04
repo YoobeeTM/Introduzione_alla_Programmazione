c7187223a78b97ed1afaecfda2bfd91d397f3581
YoobeeTM <ziocaccaputo@gmail.com>
2025-12-04 11:12:27 +0100

    finito lab9

--- FarahA-LAB9/phoneBook.cpp
+++ FarahA-LAB9/phoneBook.cpp
@@ -1,22 +1,125 @@
 #include "phoneBook.h"
-
+#include <iostream>
 using namespace std;
 
 void push_contact_in_phoneBook(phoneBook &B, const string &surname, const string &name, const string &number)
 {
-    
+    contact C;
+    C.surname=surname;
+    C.name=name;
+    C.phoneNumber=number;
+    B.push_back(C);
+
 }
 
-void print_phoneBook(const phoneBook &B);
+void print_phoneBook(const phoneBook &B)
+{
+    for (size_t i=0; i<B.size(); i++)
+    {
+        cout<<B[i].surname<<"  "<<B[i].name<<"  "<<B[i].phoneNumber<<endl;
+    }
+}
 
-void read_phoneBook_from_file(phoneBook &B, const string &fileName);
+void read_phoneBook_from_file(phoneBook &B, const string &fileName)
+{
+    ifstream fin(fileName);
 
-void write_phoneBook_to_file(const phoneBook &B, const string &fileName);
+    B.clear();    // svuota la rubrica
 
-void sort_phoneBook_by_surnames(phoneBook &B);
+    size_t N;
+    fin >> N;     // legge il numero di contatti
 
-bool find_phoneBook_contact_by_surname(const phoneBook &B, const string &s, size_t &pos);
+    for (size_t i = 0; i < N; ++i) {
+        contact c;
+        fin >> c.surname >> c.name >> c.phoneNumber;
+        B.push_back(c);
+    }
 
-void shift_phoneBook(phoneBook &B, size_t pos);
+}
 
-void add_contact_to_phoneBook(phoneBook &B, const string &surname, const string &name, const string &number);
+void write_phoneBook_to_file(const phoneBook &B, const string &fileName)
+{
+    ofstream fout(fileName);
+
+    fout << B.size() << endl;
+
+    for (const auto &c : B) {
+        fout << c.surname << " "
+             << c.name << " "
+             << c.phoneNumber << endl;
+    }
+}
+
+void sort_phoneBook_by_surnames(phoneBook &B)
+{
+    for (size_t i = 0; i < B.size(); i++) {
+        for (size_t j = 0; j < B.size() - 1; j++) {
+
+            if (B[j].surname > B[j + 1].surname) {
+                contact temp = B[j];
+                B[j] = B[j + 1];
+                B[j + 1] = temp;
+            }
+
+        }
+    }
+}
+
+bool find_phoneBook_contact_by_surname(const phoneBook &B, const string &S, size_t &pos)
+{
+    size_t left = 0;
+    size_t right = B.size();
+
+    while (left < right) {
+        size_t mid = (left + right) / 2;
+
+        if (B[mid].surname == S) {
+            pos = mid;
+            return true;
+        }
+
+        if (B[mid].surname < S)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    pos = left;
+    return false;
+}
+
+void shift_phoneBook(phoneBook &B, size_t pos)
+{
+    // se pos è alla fine (o oltre) basta aggiungere un elemento vuoto
+    if (pos >= B.size()) {
+        contact c;
+        B.push_back(c);
+        return;
+    }
+
+    // aggiungo una copia dell'ultimo elemento per avere spazio
+    B.push_back(B.back());
+
+    // sposto a destra gli elementi (attenzione all'ordine: dal fondo verso pos)
+    for (size_t i = B.size() - 2; i > pos; --i) {
+        B[i + 1] = B[i];
+    }
+    // i == pos
+    B[pos + 1] = B[pos];
+}
+
+void add_contact_to_phoneBook(phoneBook &B, const string &surname, const string &name, const string &number)
+{
+    size_t pos;
+    // trova posizione di inserimento (pos sarà dove inserire se non trovato,
+    // o l'indice di un elemento con lo stesso surname se trovato)
+    bool found = find_phoneBook_contact_by_surname(B, surname, pos);
+
+    // se vogliamo inserire prima dell'elemento trovato (o nella posizione di inserimento)
+    shift_phoneBook(B, pos);
+
+    // scrivo il nuovo contatto nella posizione libera
+    B[pos].surname = surname;
+    B[pos].name = name;
+    B[pos].phoneNumber = number;
+}
--- FarahA-LAB9/phoneBook_main_test.cpp
+++ FarahA-LAB9/phoneBook_main_test.cpp
@@ -1,1 +1,89 @@
+// phoneBook_main_test.cpp
+#include <iostream>
 #include "phoneBook.h"
+
+using namespace std;
+
+void printBook(const phoneBook &B)
+{
+    if (B.empty()) {
+        cout << "Rubrica vuota\n";
+        return;
+    }
+
+    for (size_t i = 0; i < B.size(); ++i) {
+        cout << i << ": "
+             << B[i].surname << " "
+             << B[i].name << " "
+             << B[i].phoneNumber << "\n";
+    }
+}
+
+int main()
+{
+    // 1) Creo una rubrica di prova e la scrivo su file
+    phoneBook B;
+
+    contact c1; c1.surname = "Einstein"; c1.name = "Albert"; c1.phoneNumber = "012334454";
+    contact c2; c2.surname = "Tesla";     c2.name = "Nikola"; c2.phoneNumber = "088388831";
+    contact c3; c3.surname = "Gauss";     c3.name = "Carl-Friedrich"; c3.phoneNumber = "03966776111";
+    contact c4; c4.surname = "Fibonacci"; c4.name = "Leonardo"; c4.phoneNumber = "11235813";
+
+    B.push_back(c1);
+    B.push_back(c2);
+    B.push_back(c3);
+    B.push_back(c4);
+
+    cout << "Rubrica iniziale (creata in memoria):\n";
+    printBook(B);
+    cout << "\n";
+
+    // scrivo su file
+    write_phoneBook_to_file(B, "phonebook.txt");
+    cout << "Rubrica salvata in 'phonebook.txt'.\n\n";
+
+    // 2) svuoto e rileggo dal file
+    B.clear();
+    read_phoneBook_from_file(B, "phonebook.txt");
+    cout << "Rubrica letta da 'phonebook.txt':\n";
+    printBook(B);
+    cout << "\n";
+
+    // 3) ordino per cognome
+    sort_phoneBook_by_surnames(B);
+    cout << "Rubrica dopo sort per cognome:\n";
+    printBook(B);
+    cout << "\n";
+
+    // 4) ricerca binaria: caso trovato
+    size_t pos;
+    bool found = find_phoneBook_contact_by_surname(B, "Gauss", pos);
+    if (found) {
+        cout << "Trovato 'Gauss' in posizione " << pos << ": "
+             << B[pos].surname << " " << B[pos].name << " " << B[pos].phoneNumber << "\n";
+    } else {
+        cout << "'Gauss' non trovato, posizione di inserimento: " << pos << "\n";
+    }
+    cout << "\n";
+
+    // 5) ricerca binaria: caso non trovato
+    found = find_phoneBook_contact_by_surname(B, "Bianchi", pos);
+    if (found) {
+        cout << "Trovato 'Bianchi' in posizione " << pos << "\n";
+    } else {
+        cout << "'Bianchi' non trovato, posizione di inserimento: " << pos << "\n";
+    }
+    cout << "\n";
+
+    // 6) inserimento di un nuovo contatto (mantiene l'ordine)
+    add_contact_to_phoneBook(B, "Bernoulli", "Jacob", "099999999");
+    cout << "Dopo add_contact_to_phoneBook(B, \"Bernoulli\", ...):\n";
+    printBook(B);
+    cout << "\n";
+
+    // 7) salvo la rubrica aggiornata
+    write_phoneBook_to_file(B, "phonebook_updated.txt");
+    cout << "Rubrica aggiornata salvata in 'phonebook_updated.txt'.\n";
+
+    return 0;
+}
